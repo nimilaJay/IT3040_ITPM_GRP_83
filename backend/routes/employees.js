b86b8948c3fd0cb4039f707a746a03cc01295643
Malith Lakshan <77924954+wijesekara12@users.noreply.github.com>
2023-03-25 11:58:32 +0530

    Update employee route

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -31,88 +31,6 @@ router.post("/employees/save", async (req, res) => {
 
 
 
-
-
-
-//Get a count of employees based on status [Senior/Trainee] for employee report
-router.get("/employees/division", (req, res) => {  
-  employees.find({status : { $eq: "Senior" }}).count().exec((err, senior) => { employees.find({status : { $eq: "Trainee" }}).count().exec((err, trainee) => { 
-    if (err) {
-      return res.status(400).json({ success: false, err });
-    }
-      return res.status(200).json({
-        success: true,
-        seniorcount: senior,
-        traineecount: trainee,
-     })     
-    });
-  });
-});
-
-
-//Get a list of employeees in each province for employee report
-router.get("/employees/province", (req, res) => {  
-  employees.aggregate([
-    { "$facet": {
-       "W": [
-      { "$match": { "province": "Western" }},
-      { "$count": "W" }
-      ],
-      "C": [
-      { "$match": { "province": "Central" }},
-      { "$count": "C" }
-      ],
-      "S": [
-      { "$match": { "province": "Southern" }},
-      { "$count": "S" }
-      ],
-      "U": [
-      { "$match": { "province": "Uva" }},
-      { "$count": "U" }
-      ],
-      "Sa": [
-      { "$match": { "province": "Sabaragamuwa" }},
-      { "$count": "Sa" }
-      ],
-      "NW": [
-      { "$match": { "province": "North Western" }},
-      { "$count": "NW" }
-      ],
-      "NC": [
-      { "$match": { "province": "North Central" }},
-      { "$count": "NC" }
-      ],
-      "N": [
-      { "$match": { "province": "Northern" }},
-      { "$count": "N" }
-      ],
-      "E": [
-      { "$match": { "province": "Eastern" }},
-      { "$count": "E" }
-      ]
-    }},
-      { "$project": {
-      "W": { "$arrayElemAt": ["$W.W", 0] },
-      "C": { "$arrayElemAt": ["$C.C", 0] },
-      "S": { "$arrayElemAt": ["$S.S", 0] },
-      "U": { "$arrayElemAt": ["$U.U", 0] },
-      "Sa": { "$arrayElemAt": ["$Sa.Sa", 0] },
-      "NW": { "$arrayElemAt": ["$NW.NW", 0] },
-      "NC": { "$arrayElemAt": ["$NC.NC", 0] },
-      "N": { "$arrayElemAt": ["$N.N", 0] },
-      "E": { "$arrayElemAt": ["$E.E", 0] }
-    }}
-    ]).exec((err, province) => { 
-    if (err) {
-      return res.status(400).json({ success: false, err });
-    }
-      return res.status(200).json({
-        success: true,
-        province: province,
-     })     
-    });
-});
-
 //Get a all the emloyees and assignments
 router.get("/employeepoints", (req, res) => {
   let empno = req.params.name;
@@ -128,37 +46,6 @@ router.get("/employeepoints", (req, res) => {
         l: l,
         l2:l2,
       });
-    });
-  });
-});
-
-
-
-//=======================Get Specific Routes====================================
-//Get the details of a specific employee for employee profile
-router.get("/employees/:id", (req, res) => {
-  let postid = req.params.id;
-  employees.findById(postid, (err, employee) => {
-    if (err) {
-      return res.status(400).json({ success: false, err });
-    }
-    return res.status(200).json({
-      success: true,
-      employee,
-    });
-  });
-});
-
-//Get the total allocations a specific employee had for the employee profile 
-router.get("/employees/alocation/:id1", (req, res) => {
-  let postid = req.params.id1;
-  assignment_assignedtostaff.find({emp_no :postid}).count().exec((err, allocationcount) => {
-    if (err) {
-      return res.status(400).json({ success: false, err });
-    }
-    return res.status(200).json({
-      success: true,
-      allocationcount,
     });
   });
 });
