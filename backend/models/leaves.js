9949a1ff48993adea8af2973fc8f641f003fd8da
Malith Lakshan <77924954+wijesekara12@users.noreply.github.com>
2023-05-20 00:23:44 +0530

    Add leaves model

---
+++ backend/models/leaves.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+//changed
+const leavesSchema = new mongoose.Schema({
+    //declare variables/properties
+    empno: {
+        type:Number,
+        required:true,//since must have
+    },
+    name: {
+        type: String,
+        required: false,
+    },
+    leave_type: {
+        type: String,
+        required: false,
+    },
+    
+    //leave dates
+    leave_start_date: {
+        type: String,
+        required: false,
+    },
+    leave_end_date: {
+        type: String,
+        required: false,
+    },
+
+    no_of_leaves: {
+        type: Number,
+        required: false,
+    },
+    leave_message: {
+        type: String,
+        required: false,
+    },
+    leave_status: {
+        type: String,
+        required: false,
+    },
+
+});
+
+module.exports = mongoose.model("leaves", leavesSchema);
