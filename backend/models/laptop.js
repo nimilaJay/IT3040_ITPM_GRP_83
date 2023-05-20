71060f0a6f4977070819de88c976e71ab2153f5c
Thilanka Dilshan <88785434+it20603854@users.noreply.github.com>
2023-05-20 12:31:55 +0530

    Add laptop model

---
+++ backend/models/laptop.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+//laptop ba
+const laptop_Schema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  storage_type: {
+    type: String,
+    required: false,
+  },
+  purchaase_date: {
+    type: String,
+    required: false,
+
+  },
+  purchase_price: {
+    type: Number,
+    required:false,
+
+  },
+ 
+  discarded_reason: {
+    type: String,
+    required:false,
+
+  },
+  discarded_date: {
+    type: String,
+    required:false,
+
+  },
+  update_partner_id: {
+    type: String,
+    required:false,
+
+  },
+  
+  status:{
+    type: String,
+    requires: false,
+  }
+
+});
+module.exports = mongoose.model("laptop",laptop_Schema);
+
