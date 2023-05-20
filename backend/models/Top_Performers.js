fe757cd0339e93010fd2f45c7eab9a959910d299
Thilanka Dilshan <88785434+it20603854@users.noreply.github.com>
2023-05-20 13:24:50 +0530

    update topPermers

---
+++ backend/models/Top_Performers.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const top_Performers_Schema = new mongoose.Schema({
+  
+    /*notice_top_id:{
+      type: String,
+      required: true,
+    },*/
+    year:{
+      type:Number,
+      required: false
+    },
+    month:{
+      type:String,
+      required: false
+    },
+    rank1:{
+      type: Number,
+      required: false
+    },
+    top_empid1:{
+      type: String,
+      required: false
+    },
+    
+    total_points1:{
+      type: Number,
+      required: false 
+    },
+    rank2:{
+      type: Number,
+      required: false
+    },
+    top_empid2:{
+      type: String,
+      required: false
+    },
+    
+    total_points2:{
+      type: Number,
+      required: false 
+    },
+    rank3:{
+      type: Number,
+      required: false
+    },
+    top_empid3:{
+      type: String,
+      required: false
+    },
+    
+    total_points3:{
+      type: Number,
+      required: false 
+    }
+});
+
+    
+module.exports = mongoose.model('Top_Performers', top_Performers_Schema);
