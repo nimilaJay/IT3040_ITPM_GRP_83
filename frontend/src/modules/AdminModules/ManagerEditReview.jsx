349775d54130c26dbc64eb0d13a30d5913decad2
Nimila Jayawardena <88384894+nimilaJay@users.noreply.github.com>
2023-06-02 14:46:54 +0530

    Assign New Assignment

---
+++ frontend/src/modules/AdminModules/ManagerEditReview.jsx
@@ -0,0 +1,347 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { storage } from "../../firebase";
+import Progress from "../../component/common/ProgressBar/progress";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export default class ManagerEditReview extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      execid_review: "",
+      report: "",
+      reportPDF: null,
+      points: "",
+      feedback: "",
+      managerStatus: "",
+      status: "Pending",
+      uploadPercentage: 0,
+      fileVal: ""
+    };
+  }
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    if (name === "execid_review") {
+      if (value.match("^[A-Z]{1}[0-9]{1,4}$")) {
+        document.getElementById("errorMessageExID").innerHTML = "";
+      } else {
+        document.getElementById("errorMessageExID").innerHTML =
+          "Please Enter correct Review ID";
+      }
+    }
+
+    if (name === "report") {
+      if (value.match("^[a-zA-Z][a-zA-Z\\s]+$")) {
+        document.getElementById("errorMessageName").innerHTML = "";
+      } else {
+        document.getElementById("errorMessageName").innerHTML =
+          "Please Enter correct Report Name";
+      }
+    }
+
+    if (name === "feedback") {
+      if (value.match("^[a-zA-Z][a-zA-Z\\s]+$")) {
+        document.getElementById("errorMessageFeed").innerHTML = "";
+      } else {
+        document.getElementById("errorMessageFeed").innerHTML =
+          "Please Enter correct Feedback";
+      }
+    }
+    console.log(name);
+    console.log(value);
+    if (name === "managerStatus") {
+      if (value === "Accepted") {
+        this.state.managerStatus = "Accepted";
+      } else if (value === "Pending") {
+        this.state.managerStatus = "Pending";
+      } else {
+        this.state.managerStatus = "Rejected";
+        this.state.status = "Rejected";
+      }
+    }
+
+    console.log(this.state.managerStatus);
+    this.setState({
+      ...this.state,
+      [name]: value
+    });
+  };
+
+  ReviewUpdate = () => {
+    toast.success("Review Updated Successfully");
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const id = this.props.match.params.id;
+    const {
+      execid_review,
+      report,
+      reportPDF,
+      points,
+      feedback,
+      managerStatus,
+      status
+    } = this.state;
+
+    if (execid_review === "" && report === "" && feedback === "") {
+      this.errorMessageAlert(
+        "You can't save anything without entering details"
+      );
+    } else if (execid_review === "") {
+      //document.getElementsByClassName('errorMessage').innerHTML = '';
+      document.getElementById("errorMessageExID").innerHTML =
+        "Enter Correct Report ID";
+    } else if (report === "") {
+      //document.getElementsByClassName('errorMessage').innerHTML = '';
+      document.getElementById("errorMessageName").innerHTML =
+        "Enter Correct Report Name";
+    } else if (feedback === "") {
+      //document.getElementsByClassName('errorMessage').innerHTML = '';
+      document.getElementById("errorMessageFeed").innerHTML =
+        "Enter Correct Feedback";
+    } else {
+      //document.getElementsByClassName('errorMessage').innerHTML = '';
+    }
+    let data = "";
+    if (managerStatus === "Accepted") {
+      data = {
+        execid_review: execid_review,
+        report: report,
+        reportPDF: reportPDF,
+        points: points,
+        feedback: feedback,
+        managerStatus: managerStatus,
+        isManagerApprove: true
+      };
+    } else {
+      data = {
+        execid_review: execid_review,
+        report: report,
+        reportPDF: reportPDF,
+        points: points,
+        feedback: feedback,
+        managerStatus: managerStatus,
+        status: status,
+        isManagerApprove: false
+      };
+    }
+    console.log(data);
+    console.log(status);
+
+    axios.put(`http://localhost:5000/review/update/${id}`, data).then(res => {
+      if (res.data.success) {
+        this.ReviewUpdate("Review Updated Successfully");
+        let managerReview = data;
+
+        this.setState({
+          execid_review: "",
+          report: "",
+          reportPDF: null,
+          points: "",
+          feedback: "",
+          managerStatus: ""
+        });
+      }
+    });
+    this.props.history.push("/managerreview");
+  };
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    axios.get(`http://localhost:5000/review/${id}`).then(res => {
+      if (res.data.success) {
+        this.setState({
+          execid_review: res.data.post.execid_review,
+          report: res.data.post.report,
+          reportPDF: res.data.post.reportPDF,
+          points: res.data.post.points,
+          feedback: res.data.post.feedback,
+          managerStatus: res.data.post.managerStatus
+        });
+      }
+    });
+  }
+
+  uploadPDF(e) {
+    if (e.target.files[0] !== null) {
+      const uploadTask = storage
+        .ref(`users/${e.target.files[0].name}`)
+        .put(e.target.files[0]);
+      uploadTask.on(
+        "state_changed",
+        snapshot => {
+          //progress function
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          this.setState({ uploadPercentage: progress });
+        },
+        error => {
+          //error function
+          console.log(error);
+        },
+        () => {
+          //complete function
+          storage
+            .ref("users")
+            .child(e.target.files[0].name)
+            .getDownloadURL()
+            .then(url => {
+              this.setState({ reportPDF: url });
+              console.log("Hello " + url);
+            });
+        }
+      );
+    } else {
+    }
+  }
+  render() {
+    return (
+      <div className="col-md-8 mt-4 mx-auto">
+        <h1 className="h3 mb-3 font-weight-normal">Edit Manager Review</h1>
+        <form className="need-validation" noValidate>
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Review Id</label>
+            <input
+              type="text"
+              className="form-control"
+              name="execid_review"
+              placeholder="Edit Review Id"
+              value={this.state.execid_review}
+              onChange={this.handleInputChange}
+              disabled
+            />
+            <span id="errorMessageExID" style={{ color: "red" }}></span>
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Report Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="report"
+              placeholder="Edit Report Name"
+              value={this.state.report}
+              onChange={this.handleInputChange}
+              disabled
+            />
+            <span id="errorMessageName" style={{ color: "red" }}></span>
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>
+              Upload Your Report PDF
+            </label>
+
+            <input
+              type="file"
+              id="file"
+              className="form-control"
+              name="reportPDF"
+              // value={this.state.reportPDF}
+              onChange={e => {
+                this.uploadPDF(e);
+              }}
+              multiple=""
+              disabled
+            />
+            <div className="row d-flex justify-content-end mt-3">
+              <a
+                href={this.state.reportPDF}
+                className="btn btn-primary col-2 me-2"
+              >
+                View PDF
+              </a>
+            </div>
+          </div>
+
+          <div className="mt-3">
+            <Progress percentage={this.state.uploadPercentage} />
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Report Points</label>
+            <select
+              defaultValue={"DEFAULT"}
+              className="form-select"
+              aria-label="Default select example"
+              onChange={this.handleInputChange}
+              name="points"
+            >
+              <option value="DEFAULT" disabled>
+                selected Point is : {this.state.points}
+              </option>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Feedback</label>
+            <input
+              type="text"
+              className="form-control"
+              name="feedback"
+              placeholder="Edit Feedback"
+              value={this.state.feedback}
+              onChange={this.handleInputChange}
+            />
+            <span id="errorMessageFeed" style={{ color: "red" }}></span>
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Report Status</label>
+            <select
+              defaultValue={"DEFAULT"}
+              className="form-select"
+              aria-label="Default select example"
+              onChange={this.handleInputChange}
+              name="managerStatus"
+            >
+              <option value="DEFAULT" disabled>
+                selected status is : {this.state.managerStatus}
+              </option>
+              <option value="Pending">Pending</option>
+              <option value="Rejected">Rejected</option>
+              <option value="Accepted">Accepted</option>
+            </select>
+          </div>
+
+          <div class="d-flex justify-content-center">
+            <button
+              className="btn btn-info"
+              type="submit"
+              style={{ backgroundColor: "#1687A7" }}
+              onClick={this.onSubmit}
+            >
+              &nbsp;&nbsp;Save&nbsp;&nbsp;
+            </button>{" "}
+            &nbsp;&nbsp;
+            <ToastContainer
+              position="top-center"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme={"dark"}
+              type="success"
+            />
+            <button className="btn btn-danger" type="cancel">
+              <a href="/managerreview" style={{ textDecoration: "none" }}>
+                Cancel
+              </a>
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/modules/AdminModules/ManagerReview.jsx
@@ -0,0 +1,300 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Clock from "../../component/common/clock/Clock";
+import "./Review.css";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ReactTooltip from "react-tooltip";
+export default class AdminTab1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    this.retrievePosts();
+  }
+
+  retrievePosts() {
+    axios.get("http://localhost:5000/review").then(res => {
+      if (res.data.success) {
+        const acceptedData = res.data.existingPosts.filter(
+          x => x.init_status === "Accepted" && x.isAdminApprove === true
+        );
+
+        this.setState({
+          posts: acceptedData
+        });
+      }
+    });
+  }
+
+  notify = () => {
+    toast.success("Deleted Successfully !!!");
+  };
+
+  onDelete = id => {
+    axios.delete(`http://localhost:5000/review/delete/${id}`).then(res => {
+      this.retrievePosts();
+      this.notify();
+    });
+  };
+
+  filterData(posts, searchKey) {
+    const result = posts.filter(
+      post =>
+        post.execid_review.toLowerCase().includes(searchKey) ||
+        post.report.toLowerCase().includes(searchKey) ||
+        post.feedback.toLowerCase().includes(searchKey) ||
+        post.status.toLowerCase().includes(searchKey)
+    );
+    this.setState({ posts: result });
+  }
+
+  handleSearchArea = e => {
+    const searchKey = e.currentTarget.value;
+
+    axios.get("http://localhost:5000/review").then(res => {
+      if (res.data.success) {
+        this.filterData(res.data.existingPosts, searchKey);
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="container ">
+        <div class="adminpayroll react-bs-table-pagination">
+          <div className="row">
+            <div className="d-flex justify-content-between">
+              <div className="col-lg-9 mt-2 mb-2 font-weight-bold ">
+                <br />
+                <h1 class="ap-topic">User Executive Management Dashboard</h1>
+              </div>
+            </div>
+            <hr />
+            <div className="col-lg-9 mt-2 mb-2">
+              <Clock />
+              <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <div class="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Review As a
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="/managerreview">
+                    Manager
+                  </a>
+                  <a class="dropdown-item" href="/directorreview">
+                    Director
+                  </a>
+                  <a class="dropdown-item" href="/partnerreview">
+                    Partner
+                  </a>
+                </div>
+              </div>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <button className="btn btn-lg aptab-btn">
+                <a
+                  href="/admin"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Back to Main Dashboard
+                </a>
+              </button>
+            </div>
+            <div className="col-lg-3 mt-2 mb-2">
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-9 mt-2 mb-2">
+              <h1 className="h3 mb-3 font-weight-normal">Manager Reviews</h1>
+            </div>
+            <div className="col-lg-2 mt-2 mb-2 search-bar">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="search"
+                name="searchQuery"
+                onChange={this.handleSearchArea}
+              ></input>
+            </div>
+          </div>
+          <table
+            className="table table-hover text-center"
+            style={{ marginTop: "40px" }}
+          >
+            <thead class="tblhead">
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">Review Id</th>
+                <th scope="col">Report Name</th>
+                <th scope="col">Report</th>
+                <th scope="col">Points</th>
+                <th scope="col">Feedback</th>
+                <th scope="col">Status</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.posts.map((posts, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>
+                    <a
+                      href={`/post/${posts._id}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      {posts.execid_review}
+                    </a>
+                  </td>
+                  <td>{posts.report}</td>
+                  <td>
+                    <a
+                      href={posts.reportPDF}
+                      style={{ textDecoration: "none" }}
+                    >
+                      View Report
+                    </a>
+                  </td>
+                  <td>{posts.points}</td>
+                  <td>{posts.feedback}</td>
+
+                  {posts.managerStatus === "Accepted" && (
+                    <td>
+                      <a
+                        href={`/editmanagerreview/${posts._id}`}
+                        style={{ textDecoration: "none" }}
+                        data-tip
+                        data-for="EditMan"
+                      >
+                        <b style={{ color: "green" }}>Accepted</b>
+                      </a>
+                    </td>
+                  )}
+                  {posts.managerStatus === "Rejected" && (
+                    <td>
+                      <a
+                        href={`/editmanagerreview/${posts._id}`}
+                        style={{ textDecoration: "none" }}
+                        data-tip
+                        data-for="EditMan"
+                      >
+                        <b style={{ color: "red" }}>Rejected</b>
+                      </a>
+                    </td>
+                  )}
+                  {posts.managerStatus === "Pending" && (
+                    <td>
+                      <a
+                        href={`/editmanagerreview/${posts._id}`}
+                        style={{ textDecoration: "none" }}
+                        data-tip
+                        data-for="EditMan"
+                      >
+                        <b style={{ color: "blue" }}>Pending</b>
+                      </a>
+                    </td>
+                  )}
+                  {/* <td>
+                    <a
+                      href={`/editmanagerreview/${posts._id}`}
+                      style={{ textDecoration: "none" }}
+                      data-tip
+                      data-for="EditMan"
+                    >
+                      {posts.managerStatus}
+
+                    </a>
+                    <ReactTooltip id="EditMan" place="top">
+                      <span>Edit Manager Review</span>
+                    </ReactTooltip>
+                  </td> */}
+
+                  <td>
+                    <a href={`/post/${posts._id}`} data-tip data-for="ViewMan">
+                      <i class="far fa-eye"></i>
+                    </a>
+                    <ReactTooltip id="ViewMan" place="top">
+                      <span>View Manager Review</span>
+                    </ReactTooltip>
+                    &nbsp; &nbsp; &nbsp; &nbsp;
+                    <a
+                      href={`/editmanagerreview/${posts._id}`}
+                      data-tip
+                      data-for="EditMan"
+                    >
+                      <i class="far fa-edit"></i>
+                    </a>
+                    <ReactTooltip id="EditMan" place="top">
+                      <span>Edit Manager Review</span>
+                    </ReactTooltip>
+                    &nbsp; &nbsp; &nbsp;
+                    <a
+                      href="#"
+                      data-tip
+                      data-for="DeleteMan"
+                      onClick={() =>
+                        confirmAlert({
+                          title: "Confirm to Delete",
+                          message: "Are you sure to do this ?",
+                          buttons: [
+                            {
+                              label: "Yes",
+                              onClick: () => this.onDelete(posts._id)
+                            },
+                            {
+                              label: "No",
+                              onClick: () => window.close
+                            }
+                          ]
+                        })
+                      }
+                    >
+                      <i class="far fa-trash-alt"></i>
+                    </a>
+                    <ReactTooltip id="DeleteMan" place="top">
+                      <span>View Manager Review</span>
+                    </ReactTooltip>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {/* <button className="btn btn-success">
+            <a href="/add" style={{ textDecoration: "none", color: "white" }}>
+              Create New Review
+            </a>
+          </button> */}
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme={"dark"}
+            type="success"
+          />
+        </div>
+      </div>
+    );
+  }
+}
