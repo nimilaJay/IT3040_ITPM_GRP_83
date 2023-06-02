cb813e6a7c28b61713e20322208ddfbb0c7fb2d4
Thilanka Dilshan <88785434+it20603854@users.noreply.github.com>
2023-06-02 14:49:20 +0530

    Allo new Inventory

---
+++ frontend/src/modules/AdminModules/LapAllo.jsx
@@ -0,0 +1,177 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./LapAllo.css";
+import { confirmAlert } from "react-confirm-alert"; // Imports for confirm alert
+import "react-confirm-alert/src/react-confirm-alert.css";
+import ReactTooltip from "react-tooltip"; // Imports for tooltip
+
+export default class LapAllo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lapassignments: []
+    };
+  }
+  //retrirve data
+  componentDidMount() {
+    this.retrievePosts();
+  }
+  //retrieve function for allocations
+  retrievePosts() {
+    axios.get("http://localhost:5000/lapassignments/dis").then(res => {
+      if (res.data.success) {
+        this.setState({
+          lapassignments: res.data.lapassignments,
+          l: res.data.lapassignments.length
+        });
+        console.log(this.state.lapassignments);
+      }
+    });
+  }
+  //delete function
+  onDelete = name => {
+    console.log(name);
+    axios
+      .delete(`http://localhost:5000/lapassignments/delete/${name}`)
+      .then(res => {
+        alert("Deleted Succesfully");
+        this.retrievePosts();
+      });
+  };
+  //search filter
+  filterData(lapassignmentsassigned, searchKey) {
+    console.log(searchKey);
+    const result = lapassignmentsassigned.filter(
+      lapassignmentsassigned =>
+        lapassignmentsassigned.assignment_name
+          .toLowerCase()
+          .includes(searchKey) ||
+        lapassignmentsassigned.lapid.toLowerCase().includes(searchKey)
+    );
+    this.setState({ lapassignments: result });
+  }
+  //search function
+  handleSearchArea = e => {
+    const searchKey = e.currentTarget.value;
+    axios.get("http://localhost:5000/lapassignments/dis").then(res => {
+      if (res.data.success) {
+        console.log(res.data.lapassignments);
+        this.filterData(res.data.lapassignments, searchKey);
+      }
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        <div class="main2">
+          <h2 class="head1l">Laptop Allocation</h2>
+          <a href="/lapalloreport" class="btn btn-info reportdiv">
+            <i class="fa fa-file fa-2x" aria-hidden="true"></i>&nbsp;
+          </a>
+          <hr class="line1l"></hr>
+          <a href="/allassignments">
+            <button class="div1331">
+              <p class="txt1x1" style={{ paddingTop: "11px" }}>
+                Assignments
+              </p>
+            </button>
+          </a>
+          <a href="/laptopallocation">
+            <button class="div2332">
+              <p class="txt2x2" style={{ paddingTop: "11px" }}>
+                Allocate Laptops
+              </p>
+            </button>
+          </a>
+          <div class="div3112">
+            <p class="txt354">Filter by</p>
+            <input
+              class="select121x"
+              type="text"
+              placeholder="Search..."
+              name="searchQuery"
+              onChange={this.handleSearchArea}
+            />
+          </div>
+          <h2 class="tah121">Total Laptop Allocations({this.state.l})</h2>
+          <table className="table table-hover table121">
+            <thead class="thead121">
+              <tr>
+                <th scope="col">Assignment Name</th>
+                <th scope="col">Staff ID</th>
+                <th scope="col">Laptop ID</th>
+                <th scope="col">Allocated Date</th>
+                <th scope="col">Due Date</th>
+                <th scope="col">Status</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody class="tbody121">
+              {this.state.lapassignments.map((lapassignments, index) => (
+                <tr key={index}>
+                  <td>{lapassignments.assignment_name}</td>
+                  <td>{lapassignments.empno}</td>
+                  <td>{lapassignments.lapid}</td>
+                  <td>{lapassignments.date_allocated}</td>
+
+                  <td>{lapassignments.date_received}</td>
+                  <td>
+                    <b>{lapassignments.status}</b>
+                  </td>
+
+                  <td>
+                    <a
+                      href={`/editlapallo/${lapassignments.assignment_name}`}
+                      data-tip
+                      data-for="allocationEditTip"
+                    >
+                      <i className="fas fa-edit"></i>&nbsp;
+                    </a>
+                    &nbsp;
+                    <ReactTooltip id="allocationEditTip" place="top">
+                      <span>Edit Allocation</span>
+                    </ReactTooltip>
+                    &nbsp;
+                    <a
+                      href="#"
+                      onClick={() =>
+                        confirmAlert({
+                          title: "Confirm to Delete",
+                          message: "Are you sure to delete ?",
+                          buttons: [
+                            {
+                              label: "Yes",
+                              onClick: () =>
+                                this.onDelete(lapassignments.assignment_name)
+                            },
+                            {
+                              label: "No",
+                              onClick: () => window.close
+                            }
+                          ]
+                        })
+                      }
+                      data-tip
+                      data-for="allocationDeleteTip"
+                    >
+                      <i className="far fa-trash-alt"></i>&nbsp;
+                    </a>
+                    <ReactTooltip id="allocationDeleteTip" place="top">
+                      <span>Delete Allocation</span>
+                    </ReactTooltip>
+                    &nbsp;
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot class="tfoot121">
+              <a href="/createlapallocation">
+                <i class="fas fa-plus"></i>&nbsp;New Allocation
+              </a>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/modules/AdminModules/LaptopRepair.jsx
@@ -0,0 +1,241 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./LaptopInventory.css";
+import Clock from "../../component/common/clock/Clock";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+
+export default class LaptopRepair extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      laptopsRepair: []
+    };
+  }
+
+  componentDidMount() {
+    this.retrievePosts();
+  }
+
+  printData() {
+    let printTable = document.getElementById("laptopRepairdetails").innerHTML;
+    let getprintout = window.open("", "", "height=1024, width=1024");
+    getprintout.document.write(
+      "<html><head><title>Laptop Repair Print Page</title></head>"
+    );
+    getprintout.document.write(
+      '<body style="font-family:Arial, Helvetica, sans-serif;font-size:10px;color:#000000";margin:100;padding:40> <h1 style="text-align:center"><u>Laptop Repair Details </u></h1><br>'
+    );
+    getprintout.document.write(
+      '<table style="width: 100%; border: 1px  solid black ;text-align: center"><hr><tbody style=" font-size: 36px; padding: 25px ;border: 1px  solid black ;text-align: center"',
+      printTable,
+      "</table><br>"
+    );
+    getprintout.document.write("...................<br>");
+    getprintout.document.write("signature");
+    getprintout.document.write("</body></html>");
+    getprintout.print();
+    getprintout.close();
+  }
+
+  retrievePosts() {
+    axios.get("http://localhost:5000/laptops_repair").then(res => {
+      if (res.data.success) {
+        this.setState({
+          laptopsRepair: res.data.existingLaptopsRepair,
+          laptoprepiarCount: res.data.repairCount
+        });
+        console.log(this.state.laptopsRepair);
+        console.log(this.state.laptoprepiarCount);
+      }
+    });
+  }
+
+  notify = () => {
+    toast.success("Deleted Laptop Repair Details Successfully! ");
+  };
+
+  onDelete = _id => {
+    console.log(_id);
+    axios
+      .delete(`http://localhost:5000/laptop_repair/delete/${_id}`)
+      .then(res => {
+        this.notify();
+        this.retrievePosts();
+      });
+  };
+
+  filterData(laptopsRepair, searchKey) {
+    const result = laptopsRepair.filter(
+      laptop_repair =>
+        laptop_repair.id.toLowerCase().includes(searchKey) ||
+        laptop_repair.repair_reason.toLowerCase().includes(searchKey) ||
+        laptop_repair.repair_date.toLowerCase().includes(searchKey) ||
+        laptop_repair.repair_cost.toLowerCase().includes(searchKey)
+    );
+    this.setState({ laptopsRepair: result });
+  }
+
+  handleSearchArea = e => {
+    const searchKey = e.currentTarget.value;
+    axios.get("http://localhost:5000/laptops_repair").then(res => {
+      if (res.data.success) {
+        this.filterData(res.data.existingLaptopsRepair, searchKey);
+      }
+    });
+  };
+
+  render() {
+    // calculate total repair cost
+    const totalcost = this.state.laptopsRepair.reduce(
+      (totalcost, laptopsRepair) =>
+        (totalcost += parseInt(laptopsRepair.repair_cost, 10)), //base
+      0 //sum
+    );
+
+    return (
+      <div className="container saj">
+        <br></br>
+        <div className="inventory react-bs-table-pagination">
+          <div className="row">
+            <div class="d-flex justify-content-between">
+              <div className="col-lg-9 mt-2 mb-2 font-weight-bold ">
+                <br />
+                <h1 className="ap-topic">
+                  Inventory Management | Laptop Repairing
+                </h1>
+              </div>
+              <div>
+                <Clock />
+              </div>
+            </div>
+
+            <hr />
+            <div className="col-lg-9 mt-2 mb-2">
+              <a
+                href="/admin"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <button className="btn btn-lg aptab-btn">Laptops</button>
+              </a>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <a href="/repairinglaptop">
+                <button className="btn btn-lg aptab-disable">
+                  Repairing Laptops
+                </button>
+              </a>
+            </div>
+
+            <div class="d-flex">
+              <div className="col-lg-9 mt-2 mb-2 ">
+                <h2 className="h3 mb-3">
+                  Total Repair Laptops ({this.state.laptoprepiarCount})
+                </h2>
+              </div>
+
+              <div className="col-lg-3 mt-2 mb-2 search-bar">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  name="searchQuery"
+                  aria-label="Search"
+                  onChange={this.handleSearchArea}
+                />
+              </div>
+            </div>
+          </div>
+          <div id="laptopRepairdetails">
+            <table
+              className="table table-hover text-center"
+              style={{ marginTop: "40px" }}
+            >
+              <thead class="tblhead">
+                <tr>
+                  <th scope="col">Laptop ID</th>
+                  <th scope="col">Repair Reason </th>
+                  <th scope="col">Repair Date</th>
+                  <th scope="col">Repair Price</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.laptopsRepair.map((laptopsRepair, index) => (
+                  <tr>
+                    <td>{laptopsRepair.id}</td>
+                    <td>{laptopsRepair.repair_reason}</td>
+                    <td>{laptopsRepair.repair_date}</td>
+                    <td>Rs. {laptopsRepair.repair_cost}</td>
+
+                    <td>
+                      <a href={`/viewrepair/${laptopsRepair._id}`}>
+                        <i class="far fa-eye"></i>
+                      </a>
+                      &nbsp; &nbsp; &nbsp; &nbsp;
+                      <a href={`/editrepair/${laptopsRepair._id}`}>
+                        <i class="far fa-edit"></i>
+                      </a>
+                      &nbsp; &nbsp; &nbsp; &nbsp;
+                      <a
+                        href="#"
+                        onClick={() =>
+                          confirmAlert({
+                            title: "Delete Confirmation",
+                            message: "Are you sure to delete this?",
+                            buttons: [
+                              {
+                                label: "Yes",
+                                onClick: () => this.onDelete(laptopsRepair._id)
+                              },
+                              {
+                                label: "No",
+                                onClick: () => window.close
+                              }
+                            ]
+                          })
+                        }
+                      >
+                        <i className="far fa-trash-alt"></i>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot></tfoot>
+            </table>
+          </div>
+          <div class="total">Total Repairing Cost : Rs. {totalcost}</div>
+          <div>
+            <button class="btn btn-primary" onClick={() => this.printData()}>
+              <i class="fa fa-print" aria-hidden="true"></i>&nbsp; PRINT
+            </button>
+            <p>
+              Click above button opens print preview with Laptop Repair Details
+            </p>
+          </div>
+
+          <a href="/createlaptoprepair">
+            <button class="addbtn">
+              <i class="fas fa-plus"></i>&nbsp;New Laptop Repair Details
+            </button>
+          </a>
+        </div>
+
+        <ToastContainer
+          position="bottom-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </div>
+    );
+  }
+}
